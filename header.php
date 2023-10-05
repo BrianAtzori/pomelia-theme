56a2a487469b238fd08055610213b10d20ed916c
BrianAtzori <brianatzori@gmail.com>
2023-10-05 15:30:10 +0200

    Finished Menu and Hero

--- header.php
+++ header.php
@@ -37,12 +37,24 @@
         </ul>
     </nav>
 
-    <section class="hero">
-        <h1>
-            <?php echo get_bloginfo('name') ?>
-        </h1>
-        <h2>
-            <?php echo get_bloginfo('description') ?>
-        </h2>
-        <button>Scopri di più</button>
+    <?php
+    if (has_post_thumbnail()) {
+        $post_thumbnail_id = get_post_thumbnail_id($post);
+        $featured_image = wp_get_attachment_image_src($post_thumbnail_id, 'full');
+    }
+    ?>
+
+    <section class="hero"
+        style="background: url('<?php echo $featured_image[0]; ?>') no-repeat center center fixed; background-size:cover;">
+        <div class="hero-content">
+            <h1>
+                <?php echo get_bloginfo('name') ?>
+            </h1>
+            <p>
+                <?php echo get_bloginfo('description') ?>
+            </p>
+        </div>
+
+        <button class="cta-button">Scopri di più</button>
+
     </section>
