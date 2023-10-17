9b6410772484c767773ad62008b9696feaf77328
BrianAtzori <brianatzori@gmail.com>
2023-10-17 08:21:59 +0200

    Styled Blog Page

--- archive.php
+++ archive.php
@@ -3,6 +3,7 @@ get_header()
     ?>
 
 <?php
+
 if (have_posts()) {
     while (have_posts()) {
         the_post();
--- front-page.php
+++ front-page.php
@@ -3,26 +3,28 @@ get_header()
     ?>
 
 <?php
-    if (has_post_thumbnail()) {
-        $post_thumbnail_id = get_post_thumbnail_id($post);
-        $featured_image = wp_get_attachment_image_src($post_thumbnail_id, 'full');
-    }
-    ?>
+if (has_post_thumbnail()) {
+    $post_thumbnail_id = get_post_thumbnail_id($post);
+    $featured_image = wp_get_attachment_image_src($post_thumbnail_id, 'full');
+}
+?>
 
-    <section class="hero"
-        style="background: url('<?php echo $featured_image[0]; ?>') no-repeat center center fixed; background-size:cover;">
-        <div class="hero-content">
-            <h1>
-                <?php echo get_bloginfo('name') ?>
-            </h1>
-            <p>
-                <?php echo get_bloginfo('description') ?>
-            </p>
-        </div>
+<section class="hero"
+    style="background: url('<?php echo $featured_image[0]; ?>') no-repeat center center fixed; background-size:cover;">
+    <div class="hero-content">
+        <h1>
+            <?php echo get_bloginfo('name') ?>
+        </h1>
+        <p>
+            <?php echo get_bloginfo('description') ?>
+        </p>
+    </div>
 
-        <button class="cta-button">Scopri di più</button>
+    <button class="cta-button"><a class="cta-button-link"
+            href="<?php echo esc_url(get_permalink(get_option('page_for_posts'))); ?>">Scopri di
+            più</a></button>
 
-    </section>
+</section>
 
 <section class="front-page-post">
     <?php
