8d5a0d72ea5458b012d135abd636468f859e4078
BrianAtzori <brianatzori@gmail.com>
2023-10-19 14:05:24 +0200

    404 page

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php
+get_header()
+    ?>
+
+<div class="front-page-post" style="height: 100vh; text-align: center;"> 
+    <h1>Errore! La pagina che hai cercato non esiste, controlla meglio o affidati al nostro menù in alto 🔗</h1>
+    <br>
+    <a href="/">Torna alla home 🔙</a>
+</div>
+
+<?php
+get_footer();
+?>
