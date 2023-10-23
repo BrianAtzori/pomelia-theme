7e5d106c9300117d793cbc1d2dc1757e05825cc5
BrianAtzori <brianatzori@gmail.com>
2023-10-23 23:11:48 +0200

    Fix comment template

--- comments.php
+++ comments.php
@@ -1,49 +1,57 @@
-<div id="comments" class="comments-area">
+<div class="comments-section">
     <?php if (have_comments()): ?>
-        <h2 class="comments-title">
-            <?php
-            printf(
-                _nx(
-                    'One thought on "%2$s"',
-                    '%1$s thoughts on "%2$s"',
-                    get_comments_number(),
-                    'comments title',
-                    'twentythirteen'
-                ),
-                number_format_i18n(get_comments_number()),
-                '<span>' . get_the_title() . '</span>'
-            );
-            ?>
-        </h2>
-        <ol class="comment-list">
+        <div class="comments-header">
+            <h2 class="comments-title">
+                <?php
+                printf(
+                    _nx(
+                        'È presente un commento per "%2$s" 👇🏻',
+                        'Ci sono %1$s commenti per "%2$s" 👇🏻',
+                        get_comments_number(),
+                        'comments title',
+                        ''
+                    ),
+                    number_format_i18n(get_comments_number()),
+                    '<span>' . get_the_title() . '</span>'
+                );
+                ?>
+            </h2>
+        </div>
+
+        <ol class="comments-list">
             <?php
             wp_list_comments(
                 array(
                     'style' => 'ol',
                     'short_ping' => true,
                     'avatar_size' => 74,
-                ));
+                )
+            );
             ?>
-        </ol><!-- .comment-list -->
+        </ol>
+
         <?php if (get_comment_pages_count() > 1 && get_option('page_comments')): ?>
             <nav class="navigation comment-navigation" role="navigation">
-
                 <h1 class="screen-reader-text section-heading">
-                    <?php _e('Comment navigation', 'twentythirteen'); ?>
+                    <?php _e('Navigazione Commenti', ''); ?>
                 </h1>
                 <div class="nav-previous">
-                    <?php previous_comments_link(__('&larr; Older Comments', 'twentythirteen')); ?>
+                    <?php previous_comments_link(__('&larr; Commenti più vecchi', '')); ?>
                 </div>
                 <div class="nav-next">
-                    <?php next_comments_link(__('Newer Comments &rarr;', 'twentythirteen')); ?>
+                    <?php next_comments_link(__('Commenti recenti &rarr;', '')); ?>
                 </div>
-            </nav><!-- .comment-navigation -->
-        <?php endif; // Check for comment navigation ?>
+            </nav>
+        <?php endif; ?>
+
         <?php if (!comments_open() && get_comments_number()): ?>
             <p class="no-comments">
-                <?php _e('Comments are closed.', 'twentythirteen'); ?>
+                <?php _e('I commenti  per questo articolo sono chiusi!', ''); ?>
             </p>
         <?php endif; ?>
-    <?php endif; // have_comments() ?>
+
+    <?php endif; ?>
+
     <?php comment_form(); ?>
-</div><!-- #comments -->
+
+</div>
